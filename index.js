49fb6e5e50d401bdedc0cf46dfb41aeb7e662b8c
AnirudhaLokhande <anirudha.lokhande@gmail.com>
2021-09-26 19:34:58 +0530

    delete-save-complete

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+//global array
+let globalTaskData = [];    
+taskContents = document.getElementById("taskContents");
+
 const addCard = () => {
     const newTaskDetails = {
         id: `${Date.now()}`,
@@ -6,8 +11,12 @@ const addCard = () => {
         type: document.getElementById("taskType").value,
         description: document.getElementById("taskDescription").value
     };
-    taskContents = document.getElementById("taskContents");
+    
     taskContents.insertAdjacentHTML('beforeend', generateTaskCard(newTaskDetails));
+    
+    globalTaskData.push(newTaskDetails);
+    saveToLocalStorage();
+
 }
 
 const generateTaskCard = ({id, url, title, type, description}) => {
@@ -15,11 +24,11 @@ const generateTaskCard = ({id, url, title, type, description}) => {
         <div class="card">
             <div class="card-header">
                 <div class="d-flex justify-content-end">
-                    <button type="button" class="btn btn-outline-info">
-                        <i class="fas fa-pencil-alt"></i>
+                    <button type="button" class="btn btn-outline-info" name=${id} onclick="editTask(this)">
+                        <i class="fas fa-pencil-alt" name=${id} onclick="editTask(this)"></i>
                     </button>
-                    <button type="button" class="btn btn-outline-danger">
-                        <i class="fas fa-trash-alt"></i>
+                    <button type="button" class="btn btn-outline-danger" name=${id} onclick="deleteTask(this)">
+                        <i class="fas fa-trash-alt" name=${id} onclick="deleteTask(this)"></i>
                     </button>
                 </div>
             </div>
@@ -36,3 +45,46 @@ const generateTaskCard = ({id, url, title, type, description}) => {
     </div>`)
 
 }
+
+const saveToLocalStorage = () => {
+    localStorage.setItem("tasky", JSON.stringify({task: globalTaskData}));
+}
+
+
+const reloadTaskCard = () => {
+    const localStorageCopy = JSON.parse(localStorage.getItem("tasky"));
+    if(localStorageCopy) {
+        globalTaskData = localStorageCopy["task"];  
+    }
+    globalTaskData.map((cardData) => {
+        taskContents.insertAdjacentHTML('beforeend', generateTaskCard(cardData));
+    })
+}
+
+
+const deleteTask = (e) => {
+    console.log(e)
+    const targetID = e.getAttribute("name");
+    // console.log(targetID)
+    globalTaskData = globalTaskData.filter((cardData) => cardData.id!==targetID);
+    // globalTaskData = removeTask;
+    console.log(globalTaskData);
+    saveToLocalStorage();
+    window.location.reload();
+}
+
+const editTask = (e) => {
+    const targetID = e.getAttribute("name");
+    const e.parentNode.parentNode.parentNode.childNodes[5].childNodes[1].setAttribute("contenteditable", "true")
+    e.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].setAttribute("contenteditable", "true")
+    e.parentNode.parentNode.parentNode.childNodes[5].childNodes[5].setAttribute("contenteditable", "true")
+    e.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].setAttribute("onclick", "saveEditTask(this)")
+    e.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].innerHTML = "SAVE CHANGES"
+    // saveToLocalStorage();
+    // window.location.reload();
+}
+
+const saveEditTask = (e) => {
+    const targetID = e.getAttribute("name");
+}
+
